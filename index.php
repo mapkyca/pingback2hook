02ac98bd2991772c83e719e32697619fa24bef08
Marcus Povey <marcus@marcus-povey.co.uk>
2013-07-31 16:11:43 +0100

    Placeholder front page

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Generic Pingback/Webmention endpoint : Pingback2Hook</title>
+    </head>
+    <body>
+        <h1>Generic Pingback/Webmention endpoint</h1>
+        <p>This is a generic pingback/webmention endpoint created by <a href="http://www.marcus-povey.co.uk">Marcus Povey</a>. It lets you easily add pingback support to static pages, query it, and notify other services via webhooks.</p>
+        <p>Go see the <a href="https://github.com/mapkyca/pingback2hook">project on github</a>.</p>
+    </body>
+</html>
