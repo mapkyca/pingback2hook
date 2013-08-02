46da98512ad3ad31f9dc2c6021739b61d9d2fe30
Marcus Povey <marcus@marcus-povey.co.uk>
2013-08-02 16:47:57 +0100

    Pingback example....

--- index.php
+++ index.php
@@ -19,5 +19,7 @@
         <h1>Generic Pingback/Webmention endpoint</h1>
         <p>This is a generic pingback/webmention endpoint created by <a href="http://www.marcus-povey.co.uk">Marcus Povey</a>. It lets you easily add pingback support to static pages, query it, and notify other services via webhooks.</p>
         <p>Go see the <a href="https://github.com/mapkyca/pingback2hook">project on github</a>.</p>
+        
+        <p><small><em>Note, this page has endpoints for 'example' (unless you've disabled them), which can use to test things if you like.</em></small></p>
     </body>
 </html>
