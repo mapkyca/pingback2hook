b829d8b99be28160f3b2395c6598a5f7881634f8
Marcus Povey <marcus@marcus-povey.co.uk>
2013-08-02 18:53:21 +0100

    Webhook ping working

--- engine/classes/pingback2hook/webhooks/Webhook.class.php
+++ engine/classes/pingback2hook/webhooks/Webhook.class.php
@@ -17,6 +17,8 @@ namespace pingback2hook\webhooks {
     use pingback2hook\core\Events as Events;
     use pingback2hook\storage\nosql\NoSQLStorage as NoSQLStorage;
     use pingback2hook\storage\nosql\CouchDB as CouchDB;
+    use pingback2hook\core\Log as Log;
+    use pingback2hook\endpoints\Endpoint as Endpoint;
     
     class Webhook {
         
@@ -24,15 +26,20 @@ namespace pingback2hook\webhooks {
         public static function init() {
             
             // Listen for save events in order to trigger webhooks
-            Events::register('mention', 'save', function($namespace, $event, &$parameters) {
+            Events::register('mention', 'save', function($namespace, $event, $parameters) {
                
                 $couch = CouchDB::getInstance();
                 
+                Log::debug("Sending webhook pings...");
+                
                 if ($endpoint = Endpoint::get($parameters['endpoint']))
                 {
+
+                    $json = json_encode($parameters);
+
                     foreach ($endpoint['webhooks'] as $url) {
                         
-                        $json = json_encode($parameters);
+                        Log::debug("Sending webhook to $url");
 
                         $headers = array(); 
                         $headers['Content-Length'] = strlen($json);
@@ -56,7 +63,7 @@ namespace pingback2hook\webhooks {
                         $result = file_get_contents($url, false, $context);
                         
                         // Save result of pingback
-                        $ping = new stdClass();
+                        $ping = new \stdClass();
                         $ping->on_uuid = $parameters['uuid'];
                         $ping->on_rev = $parameters['couch_rev'];
                         $ping->response_headers = $http_response_header;
@@ -66,6 +73,8 @@ namespace pingback2hook\webhooks {
                     }
                     
                 }
+                else 
+                    Log::error ("No endpoints could be found to send pingback, this shouldn't happen.");
                 
                 
             });
