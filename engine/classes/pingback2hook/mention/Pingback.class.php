65934109e6ea1316e22901b1a8d84d74add6e6d4
Marcus Povey <marcus@marcus-povey.co.uk>
2013-08-29 15:37:51 +0100

    Pingback now actually freakin' works!

--- engine/classes/pingback2hook/mention/Pingback.class.php
+++ engine/classes/pingback2hook/mention/Pingback.class.php
@@ -13,65 +13,64 @@
  */
 
 namespace pingback2hook\mention {
-    
+
     use pingback2hook\core\Page as Page;
-    use pingback2hook\xml\XmlParser as XmlParser;
-    use pingback2hook\core\Input as Input;
-    use pingback2hook\endpoints\Endpoint as Endpoint;
-    use pingback2hook\templates\Template as Template;
-    
+use pingback2hook\xml\XmlParser as XmlParser;
+use pingback2hook\core\Input as Input;
+use pingback2hook\endpoints\Endpoint as Endpoint;
+use pingback2hook\templates\Template as Template;
+
     class Pingback extends Mention {
-        
-        public static function init() {
-            
-            Mention::init();
-            
-            Page::create('pingback', function($page, $subpages){
-                
-                Input::set('_vt', 'xml');
-                
-                if ($endpoint = Endpoint::get($subpages[0])) {
-                
-                    if ($xml = XmlParser::unserialise(Input::getPOST()))
-                    {
-                        // Get source and target url
-                        $source_url = $xml->children[1]->children[0]->children[0]->children[0]->content; 
-                        $target_url = $xml->children[1]->children[1]->children[0]->children[0]->content; 
-                        
-                        // Do we have a source and target URL?
-                        if ($source_url && $target_url) {
 
-                            // Check we haven't already got this one registered.
-                            //if (self::isTargetRegistered($source_url, $target_url))
-                            //    throw new AlreadyRegisteredException("Target $target_url has already been registered.");
-                        
-                            // Check whether target is in source url
-                            if (!$details = self::checkSourceUrl($source_url, $target_url))
-                                throw new NoLinkFoundException("$target_url not found in $source_url");
-                        
-                            // Append configuration details
-                            $details['endpoint'] = $subpages[0];
-                 
-                            if (self::saveMention($target_url, $source_url, $details)) {
+        public static function endpoint($page, $subpages) {
 
-                                // Accept ping back
-                                Template::getInstance()->outputPage("Pingback of $target_url from $source_url", Template::v('xml-rpc/success', array('message' => "Ok")));
-                                
-                            }
-                            else
-                                throw new TargetNotSupportedException("Problem saving mention to $target_url from $source_url.");
-                        
+            Input::set('_vt', 'xml');
+
+            if ($endpoint = Endpoint::get($subpages[0])) {
+
+                if ($xml = XmlParser::unserialise(Input::getPOST())) {
+                    // Get source and target url
+                    $source_url = $xml->children[1]->children[0]->children[0]->children[0]->content;
+                    $target_url = $xml->children[1]->children[1]->children[0]->children[0]->content;
+
+                    // Do we have a source and target URL?
+                    if ($source_url && $target_url) {
+
+                        // Check we haven't already got this one registered.
+                        //if (self::isTargetRegistered($source_url, $target_url))
+                        //    throw new AlreadyRegisteredException("Target $target_url has already been registered.");
+                        // Check whether target is in source url
+                        if (!$details = self::checkSourceUrl($source_url, $target_url))
+                            throw new NoLinkFoundException("$target_url not found in $source_url");
+
+                        // Append configuration details
+                        $details['endpoint'] = $subpages[0];
+
+                        if (self::saveMention($target_url, $source_url, $details)) {
+
+                            // Accept ping back
+                            Template::getInstance()->outputPage("Pingback of $target_url from $source_url", Template::v('xml-rpc/success', array('message' => "Ok")));
                         }
                         else
-                            throw new SourceNotFoundException('Source and target variables missing.');
+                            throw new TargetNotSupportedException("Problem saving mention to $target_url from $source_url.");
                     }
                     else
-                        throw new TargetNotSupportedException('Problem parsing XML Pingback.');
+                        throw new SourceNotFoundException('Source and target variables missing.');
                 }
                 else
-                    throw new TargetNotFoundException('No endpoint of that definition specified.');
-                
-            });
+                    throw new TargetNotSupportedException('Problem parsing XML Pingback.');
+            }
+            else
+                throw new TargetNotFoundException('No endpoint of that definition specified.');
         }
+
+        public static function init() {
+
+            Mention::init();
+
+            Page::create('pingback', '\pingback2hook\mention\Pingback::endpoint');
+        }
+
     }
+
 }
